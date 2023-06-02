8cc3bb5ebf5acc2b51428a31ef5ca3c556054c87
Nikita Alpatiev <nikitaalpatiev@Nikitas-MacBook-Air.local>
2023-06-02 15:46:26 +0300

    added network connection checker

--- test_task/test_task/Sources/Services/Network/NetworkService.swift
+++ test_task/test_task/Sources/Services/Network/NetworkService.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SystemConfiguration
 
 // MARK: - Network service protocol
 
@@ -50,5 +51,31 @@ final class NetworkService: NetworkServiceProtocol {
         session.dataTask(with: url) { data, _, _ in
             completion(data)
         }.resume()
+    }
+}
+
+// MARK: - Helper for network connection status
+
+final class NetworkReachability {
+    
+    private init() {}
+    
+    static func isConnected() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else { return false }
+        
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) { return false }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        
+        return (isReachable && !needsConnection)
     }
 }
